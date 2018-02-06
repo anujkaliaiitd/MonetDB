0f8424522687f503a56acb076a3ef851b4437977
Anuj Kalia <akalia@cs.cmu.edu>
2018-02-05 17:42:30 -0700

    Improve MonetDB recompile script a bit

--- recompile.sh
+++ recompile.sh
@@ -1,7 +1,6 @@
 #sudo rm -rf /usr/local/var/monetdb5/
 make clean
 ./bootstrap
-#./configure --enable-strict=no --enable-debug=yes
 ./configure --enable-strict=no --enable-debug=no --enable-assert=no  --enable-optimize=yes
-make -j
+make -j 16
 sudo make install
